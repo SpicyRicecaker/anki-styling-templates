6ca3f49ec80b914b0815f9e992789a65b1c72e70
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-12-24 21:52:44 -0800

    good enough for now

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   plugins: [
+    // This array consists of the sass plugin config
     [
+      // Allows snowpack to compile sass files
       '@snowpack/plugin-sass',
       {
+        // Calls on native dart-sass installation in systeminstead of pure javascript sass
         native: true,
       },
     ],
+    // Allows snowpack to compile typescript files
     '@snowpack/plugin-typescript',
   ],
   installOptions: {
@@ -18,8 +22,11 @@ module.exports = {
     /* ... */
   },
   mount: {
-    // src: '/',
+    // Maps public to '/' path on server
     public: '/',
+    // Maps src to '/dist' path on server, after running all plugins
+    // e.g. scss becomes css, ts becomes js
+    // Just run `snowpack build` to have a better idea of what snowpack strucc
     src: '/dist',
   },
   alias: {
